01a1d2a614d47fb4f07b921b73b32b410cfd7725
tatra-t <tanechka.dush@gmail.com>
2023-05-19 23:55:23 +0300

    repos for mentor 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 const container = document.querySelector(".searchContainer");
 const searchUserInput = document.querySelector(".searchUser");
 const profile = document.querySelector(".profile");
+const repository = document.querySelector(".repos");
 class API {
   clientId = "aa5ad311b262cc778772";
   clientSecret = "a11c85334b90681cdd0a41783f5f449d3d681885";
@@ -40,9 +41,8 @@ class API {
 
 class UI {
   showProfile(user, userRepo) {
-    console.log(userRepo.length);
-    if (userRepo.length<5){
-
+    console.log(userRepo);
+    repository.innerHTML ="";
     
     profile.innerHTML = `
     <div class="card card-body mb-3">
@@ -67,48 +67,14 @@ class UI {
         </div>
       </div>
       <h3 class="page-heading mb-3">Latest Repos</h3>
-      <div class="repos">
-        <p> He doesn't 5 repo</p>
-      </div>
     `;
-  } else {
     
-    profile.innerHTML = `
-    <div class="card card-body mb-3">
-        <div class="row">
-          <div class="col-md-3">
-            <img class="img-fluid mb-2" src="${user.avatar_url}">
-            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
-          </div>
-          <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
-            <span class="badge badge-info">Following: ${user.following}</span>
-            <br><br>
-            <ul class="list-group">
-              <li class="list-group-item">Company: ${user.company}</li>
-              <li class="list-group-item">Website/Blog: ${user.blog}</li>
-              <li class="list-group-item">Location: ${user.location}</li>
-              <li class="list-group-item">Member Since: ${user.created_at}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <h3 class="page-heading mb-3">Latest Repos</h3>
-      <div class="repos">  
-        <ul class="repo-group">
-          <li class="repo-group-item"><a href="${userRepo[0].html_url}"> ${userRepo[0].name}</a></li>
-          <li class="repo-group-item"><a href="${userRepo[1].html_url}"> ${userRepo[1].name}</a></li>
-          <li class="repo-group-item"><a href="${userRepo[2].html_url}"> ${userRepo[2].name}</a></li>
-          <li class="repo-group-item"><a href="${userRepo[3].html_url}"> ${userRepo[3].name}</a></li>
-          <li class="repo-group-item"><a href="${userRepo[4].html_url}"> ${userRepo[4].name}</a></li>
-        </ul>
-      </div>
-    `;
+    for (let i=0; i<userRepo.length; i++) {
+      let newLine = document.createElement("li");
+      newLine.innerHTML = `<a href="${userRepo[i].html_url}">${userRepo[i].name}</a> `;
+      repository.prepend(newLine);
+     }
   }
-  }
-
   clearProfile() {
     profile.innerHTML = "";
   }
@@ -172,3 +138,11 @@ const debounce = (func, delay) => {
 
 // Event listeners
 searchUserInput.addEventListener("input", debounce(handleInput, 1000));
+
+/*<ul class="repo-group">
+          <li class="repo-group-item"><a href="${userRepo[0].html_url}"> ${userRepo[0].name}</a></li>
+          <li class="repo-group-item"><a href="${userRepo[1].html_url}"> ${userRepo[1].name}</a></li>
+          <li class="repo-group-item"><a href="${userRepo[2].html_url}"> ${userRepo[2].name}</a></li>
+          <li class="repo-group-item"><a href="${userRepo[3].html_url}"> ${userRepo[3].name}</a></li>
+          <li class="repo-group-item"><a href="${userRepo[4].html_url}"> ${userRepo[4].name}</a></li>
+        </ul>*/
